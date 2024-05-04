5764a59bbfa64b5e5de3397cb430dfc2ee0f7fab
André Luiz <andreluizgomes43@hotmail.com>
2024-05-04 20:45:46 -0300

    Commit com Classes e guias de clientes,funcionários e fornecedores já pronto e funcionando.

---
+++ src/br/com/sistema/utilitarios/Utilitarios.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.sistema.utilitarios;
+
+import java.awt.Component;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Pichau
+ */
+
+//O método LimparFormulario serve para apagar o formulário após uma adicão.
+//Enquanto houver um elemento JTextField no formulário,ele vai excluir um por um
+public class Utilitarios {
+    public void LimparFormulario(JPanel container) {
+        Component conponents[] = container.getComponents();
+       for(Component component : conponents){
+           if(component instanceof JTextField) {
+               ((JTextField)component).setText(null);
+            }
+        }
+    }
+}
